914843949d1e6427aeafdb9a9ddab4ac9da5f2a0
Jonas Kirchhoff <jonaskir@mail.upb.de>
2023-04-24 17:15:56 +0200

    Refactor workbench for easier creation of extensions

--- workbench/src/modules/utils.ts
+++ workbench/src/modules/utils.ts
@@ -1,43 +1,54 @@
 import { is } from "bpmn-js/lib/util/ModelUtil"
+import { createEntry } from "./utils/entries"
+
+export type ElementType = "bpmn:StartEvent" | "bpmn:Activity" | "..."
+
+export interface Property {
+  /** Name of the type in the associated descriptor JSON. */
+  type: string
+  /** Name of the property in the associated descriptor JSON. */
+  name: string
+  /** Human-readable name. */
+  label: string
+  /** Human-readable description. */
+  description: string
+  /** Elements which this property can be set for. */
+  applicableElements: ElementType[]
+}
+
+// ------------------------
 
 export type Element = unknown
 export type TranslateFn = (text: string) => string
 
-export interface PropertiesGroup {
-  id: string,
-  label: string,
-  type: string;
-  entries: any[]
-}
-
-export function addGroup(element: Element, group: PropertiesGroup) {
+export function addGroup(element: Element, prop: Property) {
   return (groups: unknown[]) => {
-    if (is(element, group.type)) {
+    let isApplicable = false
+
+    for (const type of prop.applicableElements) {
+      if (is(element, type)) {
+        isApplicable = true
+      }
+    }
+
+    if (isApplicable) {
       groups.push({
-        id: group.id,
-        label: group.label, // TODO wrap with "translate"?
-        entries: group.entries
+        id: "DSE",
+        label: "Decision Support Ecosystem",
+        entries: [createEntry(element, prop)],
       })
     }
+
     return groups
   }
 }
 
-export interface PropertyInput<T = string> {
-  id: string,
-  type: "text";
-  label: string,
-  description: string,
-  getValue: (element: Element) => T
-  setValue: (element: Element, moddle, modeling, value: T) => void
-}
-
 export function getExtensionElement(element: any, type: string) {
   if (!element.extensionElements) {
-    return;
+    return
   }
 
   return element.extensionElements.values.filter((extensionElement: any) => {
-    return extensionElement.$instanceOf(type);
-  })[0];
+    return extensionElement.$instanceOf(type)
+  })[0]
 }
--- workbench/src/modules/utils/entries.tsx
+++ workbench/src/modules/utils/entries.tsx
@@ -4,33 +4,55 @@ import {
 } from "@bpmn-io/properties-panel"
 import { useService } from "bpmn-js-properties-panel"
 
-import { PropertyInput } from "../utils"
+import { Property, getExtensionElement } from "../utils"
 
-export function createInput(element: Element, input: PropertyInput) {
+export function createEntry(element: Element, property: Property) {
   return {
-    id: input.id,
+    id: property.type,
     element,
     isEdited: isTextFieldEntryEdited, // TODO account for other types
-    component: (props) => {
+    component: (props: any) => {
       const { element, id } = props
 
-      const moddle = useService("moddle") // bpmnModeler.get('moddle') ??
+      const moddle = useService("moddle")
       const modeling = useService("modeling")
-      //   const translate = useService("translate")
       const debounce = useService("debounceInput")
 
-      const setValue = (value) => {
-        input.setValue(element, moddle, modeling, value)
+      const getValue = () => {
+        const extensions = getExtensionElement(
+          element.businessObject,
+          property.type
+        )
+        return extensions ? extensions[property.name] : ""
+      }
+
+      const setValue = (value: any) => {
+        const extensionElements =
+          element.businessObject.extensionElements ||
+          moddle.create("bpmn:ExtensionElements")
+
+        let ext = getExtensionElement(element.businessObject, property.type)
+
+        if (!ext) {
+          ext = moddle.create(property.type)
+          extensionElements.get("values").push(ext)
+        }
+
+        ext[property.name] = value
+
+        return modeling.updateProperties(element, {
+          extensionElements,
+        })
       }
 
       //   TODO wrap label/description with "translate"?
       return (
         <TextFieldEntry
-          id={id}
+          id={property.type}
           element={element}
-          label={input.label}
-          description={input.description}
-          getValue={() => input.getValue()}
+          label={property.label}
+          description={property.description}
+          getValue={getValue}
           setValue={setValue}
           debounce={debounce}
         />
@@ -38,3 +60,36 @@ export function createInput(element: Element, input: PropertyInput) {
     },
   }
 }
+
+// export function createInput(element: Element, input: PropertyInput) {
+//   return {
+//     id: input.id,
+//     element,
+//     isEdited: isTextFieldEntryEdited, // TODO account for other types
+//     component: (props) => {
+//       const { element, id } = props
+
+//       const moddle = useService("moddle") // bpmnModeler.get('moddle') ??
+//       const modeling = useService("modeling")
+//       //   const translate = useService("translate")
+//       const debounce = useService("debounceInput")
+
+//       const setValue = (value) => {
+//         input.setValue(element, moddle, modeling, value)
+//       }
+
+//       //   TODO wrap label/description with "translate"?
+//       return (
+//         <TextFieldEntry
+//           id={id}
+//           element={element}
+//           label={input.label}
+//           description={input.description}
+//           getValue={() => input.getValue()}
+//           setValue={setValue}
+//           debounce={debounce}
+//         />
+//       )
+//     },
+//   }
+// }
